11a7adaf8b26bc8575f99ce2ff6bb6ade476c629
Sonali Mehta <sonalimehta.333@gmail.com>
2021-04-06 23:47:19 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+### The Setup of this mini project is quite simple as it is just a single page application (SPA).
+
+# Requiremnts :
+1. Most of the required resources are already present in this repo , inside the folder named "Resources".
+2. An active internet connection is required for recources that are hosted remotely like images and jquery CDNs.
+
+## Steps
+  # Method 1 - Through zipped file
+    1. Download the zipped file of the code.
+    2. Extract the files in a directory at any location in your computer.
+    3. Open the directory "frontend_SPA-main" present inside the extrcated folder. It will contain a file index.html.
+    4. Right click on the file index.html and select the option to open file in browser.
+  
+  # Method 2 - Through git clone
+    1. Open the command line terminal of your computer.
+    2. Change path to a location where you want to keep the clone of the repo.
+    3. Run the command -> gh repo clone sonalimehta30/frontend_SPA
+    4. Visit the cloned repo path in GUI and open the index.html file in browser.
+    
+    
---
+++ main.js
@@ -0,0 +1,147 @@
+
+var app = angular.module('myApp',[])
+
+app.controller('myCtrl',function ($scope,$http,$window) {
+	console.log('hi')
+    $scope.cart_items = []
+    arr_mainList = window.main_json
+    $scope.totalProducts = arr_mainList.length
+    $scope.tags_arr = []
+
+    for (var i = arr_mainList.length - 1; i >= 0; i--) {
+        compare_at_price = parseInt(arr_mainList[i]['compare_at_price'])
+        price            = parseInt(arr_mainList[i]['price'])
+        discount = (compare_at_price - price)/compare_at_price
+        discount = Math.round(discount*100)
+        arr_mainList[i]['discount'] = discount
+        arr_mainList[i]['display_options'] = 0;
+        arr_mainList[i]['ATC_display'] = 0;
+        tag_name = arr_mainList[i].tag
+        $scope.tags_arr.push(tag_name)
+    }
+    $scope.tags_arr = [...new Set($scope.tags_arr)]
+    
+    
+    angular.element($window).bind('resize', function() {
+        check_screen_size()
+        console.log($scope.mainList)
+    });
+    function check_screen_size(){
+        $scope.split_size = 5
+        if($(window).width()>=1440){
+            $scope.split_size = 5
+        }
+        if($(window).width()>500 && $(window).width()<800){
+            $scope.split_size = 3
+        }
+        if($(window).width()<500){
+            $scope.split_size = 2
+        }
+        console.log($(window).width(),$scope.split_size)
+        $scope.mainList = chunk_arr(arr_mainList);
+
+    }
+    function chunk_arr(arr){
+        return_arr = []
+        for(x =0 ; x<arr.length ; x+=$scope.split_size){
+            main = arr.slice(x,x+$scope.split_size)
+            return_arr.push(main)
+        }
+        return return_arr
+    }
+    $scope.filter_tag = function(tag_name){
+        if(tag_name != 'all'){
+            temp_arr = angular.copy(arr_mainList)
+            temp_arr = temp_arr.filter((v)=> v.tag.toLowerCase() == tag_name.toLowerCase())
+            $scope.mainList = chunk_arr(temp_arr);
+        }else{
+            $scope.mainList = chunk_arr(arr_mainList);
+        }
+        $scope.filter_applied = 1;
+    }
+    $scope.clear_filter = function(){
+        $scope.mainList = chunk_arr(arr_mainList);
+        $scope.filter_applied = 0;
+    }
+    $scope.sort_by_price = function(order){
+        $scope.filter_applied = 1;
+        temp = angular.copy(arr_mainList)
+        if (order=='desc') {
+            temp.sort((v,d)=> (parseInt(v.price) >= parseInt(d.price))? -1 : 1)
+        }else{
+            temp.sort((v,d)=> (parseInt(v.price) < parseInt(d.price))? -1 : 1)
+        }
+        $scope.mainList = chunk_arr(temp);
+    }
+
+    $scope.size_option_index = 0
+
+    $scope.add_to_cart = function(item){
+        var cart = $(".cart_icon");
+        var current_div = $("#display_options_"+item.id);
+        var imgtodrag = $('<img width="30px" height="30px" src="'+item.image_src[0]+'"/>')
+        var position = cart.position();
+
+        if (imgtodrag) {
+            var imgclone = imgtodrag.clone()
+            .offset({
+                top: imgtodrag.offset().top,
+                left: imgtodrag.offset().left
+            })
+            .css({
+                'opacity': '0.7',
+                'position': 'absolute',
+                'height': '10vh',
+                'width': '10vh',
+                'z-index': '100'
+            })
+            .appendTo($('#display_options_'+item.id))
+            .animate({
+                'top': cart.offset().top - current_div.offset().top,
+                'left': cart.offset().left - current_div.offset().left,
+                'width': 25,
+                'height': 25,
+            }, 800, 'easeInOutExpo');
+            setTimeout(function () {
+                cart.effect("shake", {
+                    times: 1
+                }, 170);
+            }, 800);
+
+            imgclone.animate({
+                'width': 0,
+                'height': 0
+            }, function () {
+                $(this).detach()
+            });
+        }
+
+        temp = {
+            'name': item.name,
+            'vendor' : item.vendor,
+            'price'  : item.price,
+            'size' : item.options[$scope.size_option_index].value,
+            'id' : item.options[$scope.size_option_index].id,
+            'qty' : 1
+        }
+        $scope.cart_items.push(temp);
+    }
+
+    $scope.cart_actions = function(action,index){
+        switch(action){
+            case 'plus' :  
+                $scope.cart_items[index].qty += 1;
+                break;
+            case 'minus' :  
+                $scope.cart_items[index].qty -= 1;
+                break;
+            case 'delete' :  
+                $scope.cart_items.splice(index,1)
+                break;
+            default : break;
+        }
+    }
+
+    check_screen_size()
+
+})
